9a79fb62f5d181dcc4b32e51f72b2e1f4dd10115
MARTINrichard05 <richard.martin@uha.fr>
2024-12-14 13:19:30 +0100

    Client can load files and be used

--- UserGui/LocalLibs/Backend.py
+++ UserGui/LocalLibs/Backend.py
@@ -1,3 +1,11 @@
+import sys
+import os
+
+from click import command
+
+# Ajouter le répertoire parent au sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
 import socket
 import ssl
 import json
@@ -14,36 +22,38 @@ class Client:
         self.__certfile = certfile
 
         self.__context = None
-        self.__sock = None
+        self.sock = None
 
         if self.__useSSL:
             self.__context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile=self.__certfile)
             self.__context.check_hostname = False
             self.__context.verify_mode = ssl.CERT_NONE
-            self.__sock = self.__context.wrap_socket(socket.create_connection((self.__serverAddress, self.__serverPort)), server_hostname=self.__serverAddress)
+            self.sock = self.__context.wrap_socket(socket.create_connection((self.__serverAddress, self.__serverPort)), server_hostname=self.__serverAddress)
         else:
-            self.__sock = socket.create_connection((self.__serverAddress, self.__serverPort))
+            self.sock = socket.create_connection((self.__serverAddress, self.__serverPort))
 
 
     def send(self, data : bytes):
         if self.__useSSL:
-            self.__sock.sendall(data)
+            self.sock.sendall(data)
         else:
-            self.__sock.send(data)
+            self.sock.send(data)
 
     def receive(self, size : 2048):
-        return self.__sock.recv(size)
+        return self.sock.recv(size)
 
     def close(self):
-        self.__sock.close()
+        self.sock.close()
 
     @property
     def addr(self):
-        return self.__sock.getpeername()
+        return self.sock.getpeername()
 
     @property
     def useSSL(self):
         return self.__useSSL
+
+
 
 class ControlClient(Client): # this is the client for the control connection, it must use ssl
     def __init__(self, serverAddress = "localhost", serverPort = 12345, certfile = None):
@@ -52,9 +62,18 @@ class ControlClient(Client): # this is the client for the control connection, it
         self.__uid = None
         self.startListener()
         self.pingtime = None
+        self.__code = None
+        self.printbuffer = [] # this is a buffer for the gui to print the output of the backend
+        self.__dataClient = None
+        self.__dataclientRunning = False
+
+    def print(self,msg):
+        self.printbuffer.append(msg)
+
 
     def __send (self, data : str):
         super().send(data.encode('utf-8'))
+
 
     def __listener(self):
         while self.running:
@@ -66,6 +85,13 @@ class ControlClient(Client): # this is the client for the control connection, it
                 self.__handleMessages(data.decode('utf-8'))
             else:
                 pass
+            print("usefull")
+            #if self.__dataClient:
+            #    if not self.__dataClient.running:
+            #        self.print("Client de données se ferme")
+            #        self.__dataClient.end()
+            #        self.__dataClient = None
+            #        self.print("Client de données fermé (normalement)")
 
     def startListener(self):
         self.__listenerThread = Thread(target=self.__listener)
@@ -77,26 +103,56 @@ class ControlClient(Client): # this is the client for the control connection, it
         if obj['command'] == 'uidIs':
             self.__uid = obj['uid']
         elif obj['command'] == 'OUT':
-            self.running = False
+            self.print("DECONNEXION DEMANDEE PAR LE SERVEUR")
+            self.closedataclient()
+            self.selfkill()
+            self.print("DECONNECTÉ")
+
         elif obj['command'] == 'pong':
             try :
                 self.pingtime = time.time() - self.pingtime
-                print(f"Got pong from server in {self.pingtime} seconds")
+                # print(f"Got pong from server in {self.pingtime} seconds")
             except TypeError:
                 pass
+        elif obj['command'] == 'ping':
+            # print("I live !")
+            self.__send(json.dumps({"command": "pong"}))
+
         elif obj['command'] == 'DataSession':
-            print("Got node, i'm happy")
-            print(obj['data'])
+            # print("Got node, i'm happy")
+            # print(obj['data'])
+            self.print("Le serveur m'a attribué un noeud")
             # the client is happy to have a node
             # he will now connect a data client to the Master server (proxy) for fast data transfer
             self.initDataClient(obj['data']['ip'], obj['data']['port'], obj['data']['key'], self.__uid)
+        elif obj['command'] == 'EndDataSession':
+            self.print("Le serveur a mis fin à la session de données")
+            self.closedataclient()
 
 
 
     def initDataClient(self, serverAddress, serverPort, encryptionKey = None, uid = None):
-        self.__dataClient = DataClient(serverAddress, serverPort, encryptionKey, uid = uid)
+        self.__dataclientRunning = True
+        self.__dataClient = DataClient(serverAddress, serverPort, encryptionKey, uid = uid, payload = self.__code, print_callback = self.print)
         self.__dataClient.authenticate()
         self.__dataClient.startMsgListener()
+
+    def closedataclient(self):
+        if self.__dataclientRunning:
+            self.__dataclientRunning = False
+            self.print("Client de données se ferme")
+            try :
+                self.__dataClient.running = False
+                print(1)
+                self.__dataClient.end()
+                print(2)
+            except:
+                pass
+
+            time.sleep(0.5)
+            self.__dataClient = None
+            self.print("Client de données Fermé")
+
 
 
     def requestUid(self):
@@ -106,6 +162,34 @@ class ControlClient(Client): # this is the client for the control connection, it
     def requestNode(self, RequiredPackages):
         self.__send(json.dumps({"command": "initDataSession", "RequiredPackages": RequiredPackages}))
 
+    def runCode(self, code : str):
+        self.__code = code
+        self.requestNode(self.detect_language(code))
+
+    def detect_language(self, code):
+        """
+        Detecte le langage de programmation d'un fichier en analysant son contenu.
+
+        Args:
+            code (str): Code source à analyser.
+
+        Returns:
+            str: Le langage de programmation détecté ('c', 'cpp', 'java', 'python', 'inconnu').
+        """
+        content = code
+
+        # Recherche de mots-clés pour chaque langage
+        if '#include' in content and 'stdio.h' in content:
+            return 'c'
+        elif '#include' in content and 'iostream' in content:
+            return 'cpp'
+        elif 'public class' in content:
+            return 'java'
+        elif 'print(' in content or 'def' in content:
+            return 'python'
+        else:
+            return 'inconnu'
+
     def sayBye(self):
         self.__send(json.dumps({"command": "bye", "data": str(self.__uid)}))
 
@@ -113,52 +197,117 @@ class ControlClient(Client): # this is the client for the control connection, it
         self.pingtime = time.time()
         self.__send(json.dumps({"command": "ping"}))
 
+    def selfkill(self):
+        try :
+            self.__send(json.dumps({"command": "OUT"}))
+        except:
+            pass
+        self.running = False
+        super().close()
+
     @property
     def uid(self):
         return self.__uid
 
+    @property
+    def dataclient_running(self):
+        return True if self.__dataClient else False
+
+
+
 
 
 class DataClient(Client): # this is the client for the node connection, it must not use ssl as the data will be encrypted on the application layer
-    def __init__(self, serverAddress = "localhost", serverPort = 22345, encryptionKey : str = None, payload = None, uid = None):
+    def __init__(self, serverAddress = "server", serverPort = 22345, encryptionKey : str = None, payload = None, uid = None, print_callback = None, kill_callback = None):
         super().__init__(serverAddress, serverPort, False) # no full ssl because master will serve as proxy and we d'ont want to overload it the data will be encrypted on the application layer
         self.__encryptionKey = encryptionKey
+        # print("Key:", encryptionKey)
         self.__cypher = Fernet(encryptionKey.encode('utf-8'))
         self.running = True
         self.pingtime = None
         self.__uid = uid
-        self.__payload = None
+        self.__payload = payload
         self.__auth_passed = False
+        if print_callback:
+            self.print = print_callback
+        else:
+            self.print = print
+
+        if kill_callback:
+            self.kill = kill_callback
+        else:
+            self.kill = self.close
+
+        self.print("Initialisation du client de données")
+        self.print("Le code est : " + payload)
 
     def __listener(self):
         while self.running:
             try :
-                data = self.__receive(2048)
+                # print("Listening for data")
+                data = self.__receive(4096)
+                # if the connection was closed, we will get an empty data
             except :
                 continue
             if data:
+                #print("Got data")
                 self.__handleMessages(self.__decrypt(data).decode('utf-8'))
+                #print("Handled data")
             else:
-                pass
+                self.running = False
+
+        # auto close the connection
+        print("Closing connection")
+        #self.kill()
+
+
+    def end(self):
+        self.close()
+        print("joining thread")
+        del super().sock
+        del self.__listenerThread
+        del self.__cypher
+        del self.__encryptionKey
+        del self.__uid
+        del self.__payload
+        del self.__auth_passed
+        del self
+
+        # self.__listenerThread.join()
+
 
 
     def startMsgListener(self):
         self.__listenerThread = Thread(target=self.__listener)
         self.__listenerThread.start()
-        print("DataClient started")
+        self.print("Client de données initialisé")
 
     def authenticate(self):
-        print("Authenticating")
+        self.print("Authentification en cours du client de données")
         self.__send(self.__uid.encode('utf-8'))
 
 
-
     def __handleMessages(self, data : str):
-        print(data)
-        obj = json.loads(data)
-        if obj['command'] == 'Ready':
+        # print(data)
+        try :
+            obj = json.loads(data)
+        except json.JSONDecodeError:
+            self.print("Erreur de décodage des données du noeud")
+            return
+        if obj['command'] == 'STDOUT':
+            self.STDOUT(obj['data'])
+        elif obj['command'] == 'STDERR':
+            self.STDERR(obj['data'])
+        elif obj['command'] == 'Ready':
+            if obj['data'] == self.__uid:
+                self.print("Node ready for me !")
             self.sendPayload()
-        elif obj['command'] == 'PONG':
+
+        elif obj['command'] == 'ping':
+            print("I live !")
+            self.__send(json.dumps({"command": "pong"}).encode('utf-8'))
+
+        elif obj['command'] == 'pong':
             try :
                 self.pingtime = time.time() - self.pingtime
                 print(f"Got pong from server in {self.pingtime} seconds")
@@ -166,34 +315,44 @@ class DataClient(Client): # this is the client for the node connection, it must 
                 pass
         elif obj['command'] == 'Status':
             self.updateStatus(obj['data'])
-        elif obj['command'] == 'STDOUT':
-            self.STDOUT(obj['data'])
-        elif obj['command'] == 'STDERR':
-            self.STDERR(obj['data'])
+
         elif obj['command'] == 'OUT':
+            print("Node sent OUT command")
             self.running = False
         elif obj['command'] == 'Auth_pass':
             print("Authenticated, woaw")
             self.__auth_passed = True
+        elif obj['command'] == 'PayloadExecuted':
+            self.print("Payload executed")
+            #self.sayBye()
+            #self.close()
+            self.running = False
 
 
     def updateStatus(self, status : str):
-        print(status)
+        # this one should be printed in the color 2 (green)
+        self.print(("\033[92m" + "Node Status: " +  status + "\033[0m"))
 
     def STDOUT(self, data : str):
-        print(data)
+        self.print(data)
 
     def STDERR(self, data : str):
-        print(data)
+        # we want to print it in red color
+        self.print("\033[91m" + data + "\033[0m")
 
-    def sendPayload(self, data : bytes):
-        self.__send(self.__payload)
+
+    def sendPayload(self):
+        self.print("Sending payload to node : " + self.__payload)
+        self.__send(json.dumps({"command": "Payload", "data": self.__payload}).encode('utf-8'))
 
     def __encrypt(self, data : bytes):
         return self.__cypher.encrypt(data)
 
     def __decrypt(self, data : bytes):
-        return self.__cypher.decrypt(data)
+        try:
+            return self.__cypher.decrypt(data)
+        except:
+            return b''
 
 
     def __send(self, data : bytes):
@@ -202,11 +361,24 @@ class DataClient(Client): # this is the client for the node connection, it must 
             data = self.__encrypt(data)
         super().send(data)
 
-    def __receive(self, size : 2048):
-        data = super().receive(size)
-        if self.__encryptionKey:
-            # decrypt data
-            pass
+    #def __receive(self, buffer_size=1024):
+    #    data = b''
+    #    while True:
+    #        part = super().receive(buffer_size)
+    #        data += part
+    #        if len(part) < buffer_size:
+    #            break
+    #    return data
+
+    def __receive(self, buffer_size=4096):
+        data = b''
+        while True:
+            part = super().receive(buffer_size)
+            if not part:
+                break
+            data += part
+            if len(part) < buffer_size:
+                break
         return data
 
     def sayBye(self):
---
+++ UserGui/Main.py
@@ -0,0 +1,119 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QTextEdit, QVBoxLayout, QWidget, QLineEdit, QLabel, \
+    QHBoxLayout, QFileDialog
+from PyQt6.QtCore import QTimer
+from LocalLibs.Backend import ControlClient
+import time
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Nyakloud Runner")
+
+        self.server_address_input = QLineEdit()
+        self.server_address_input.setText("localhost")
+        self.server_port_input = QLineEdit()
+        self.server_port_input.setText("12345")
+        self.connect_button = QPushButton("Connecter")
+        self.connect_button.clicked.connect(self.connect_to_server)
+
+        self.code_input = QTextEdit()
+        self.code_input.setPlaceholderText("Entrez le code à exécuter ici")
+
+        self.load_button = QPushButton("Charger un fichier")
+        self.load_button.clicked.connect(self.load_file)
+
+        self.run_button = QPushButton("Exécuter le code")
+        self.run_button.clicked.connect(self.run_code)
+        self.run_button.setEnabled(False)
+
+        self.output_display = QTextEdit()
+        self.output_display.setReadOnly(True)
+
+        top_layout = QHBoxLayout()
+        top_layout.addWidget(QLabel("Adresse:"))
+        top_layout.addWidget(self.server_address_input)
+        top_layout.addWidget(QLabel("Port:"))
+        top_layout.addWidget(self.server_port_input)
+        top_layout.addWidget(self.connect_button)
+
+        layout = QVBoxLayout()
+        layout.addLayout(top_layout)
+        layout.addWidget(self.code_input)
+        layout.addWidget(self.load_button)
+        layout.addWidget(self.run_button)
+        layout.addWidget(self.output_display)
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+        self.backend = None
+
+        # Ajouter un QTimer pour mettre à jour régulièrement l'affichage
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_output_display)
+
+    def connect_to_server(self):
+        self.connect_button.setEnabled(False)
+        if self.backend:
+            self.backend.close()
+        address = self.server_address_input.text()
+        port = int(self.server_port_input.text())
+        self.backend = ControlClient(serverAddress=address, serverPort=port, certfile="../certfile.pem")
+        self.backend.requestUid()
+        time.sleep(0.1)
+        self.output_display.append(f"Connecté au serveur avec UID: {self.backend.uid}")
+        self.run_button.setEnabled(True)
+        self.timer.start(15)
+
+    def load_file(self):
+        file_name, _ = QFileDialog.getOpenFileName(self, "Ouvrir un fichier", "", "Tous les fichiers (*)")
+        if file_name:
+            with open(file_name, 'r') as file:
+                file_content = file.read()
+                self.code_input.setPlainText(file_content)
+
+    def run_code(self):
+        self.run_button.setEnabled(False)
+        code = self.code_input.toPlainText()
+        print("Running code:", code)
+        self.backend.runCode(code)
+
+    def update_output_display(self):
+        if self.backend:
+            while self.backend.printbuffer:
+                self.output_display.append(self.backend.printbuffer.pop(0))
+
+            if self.backend.running:
+                if not self.run_button.isEnabled():
+                    self.run_button.setEnabled(True)
+                if self.connect_button.isEnabled():
+                    self.connect_button.setEnabled(False)
+            else:
+                if self.run_button.isEnabled():
+                    self.run_button.setEnabled(False)
+                if not self.connect_button.isEnabled():
+                    self.connect_button.setEnabled(True)
+
+            if self.backend.dataclient_running:
+                if self.run_button.isEnabled():
+                    self.run_button.setEnabled(False)
+            else:
+                if not self.run_button.isEnabled():
+                    time.sleep(0.5)
+                    if not self.backend.dataclient_running:
+                        self.run_button.setEnabled(True)
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
